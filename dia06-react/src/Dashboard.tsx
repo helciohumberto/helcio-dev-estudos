d1bd51d561cbd69736230f1e6ebf8f9e8bb2c5b7
Hélcio Humberto <helciohumberto@gmail.com>
2026-06-27 12:29:38 +0100

    feat: adicionar logout no dashboard

--- dia06-react/src/Dashboard.tsx
+++ dia06-react/src/Dashboard.tsx
@@ -1,5 +1,41 @@
-function Dashboard(){
-  return <h1>Página Dashboard</h1>
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom'
+
+function Dashboard() {
+  const [mensagem, setMensagem] = useState("");
+  const navigate = useNavigate()
+  useEffect(() => {
+    async function buscarDados() {
+      const token = localStorage.getItem("token");
+
+      const resposta = await fetch(
+        "https://helcio-dev-estudos-production.up.railway.app/protegido",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const dados = await resposta.json();
+      setMensagem(dados.mensagem);
+    }
+
+    buscarDados();
+  }, []);
+
+  function handleLogout(){
+    localStorage.removeItem("token")
+    navigate("/login")
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>{mensagem}</p>
+      <button onClick={handleLogout}>Sair</button>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
