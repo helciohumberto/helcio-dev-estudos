d9db98f4582ada22289b929c0d825dd94ea63fec
Hélcio Humberto <helciohumberto@gmail.com>
2026-06-30 14:56:02 +0100

    feat: aplicar tailwind css no dashboard

--- dia06-react/src/Dashboard.tsx
+++ dia06-react/src/Dashboard.tsx
@@ -30,10 +30,15 @@ function Dashboard() {
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>{mensagem}</p>
-      <button onClick={handleLogout}>Sair</button>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-gray-900">
+      <h1 className="text-3xl font-bold text-white">Dashboard</h1>
+      <p className="text-white text-sm">{mensagem}</p>
+      <button
+        className="px-4 py-2 rounded-lg bg-red-600 text-white font-semibold hover:bg-red-700 transition w-64"
+        onClick={handleLogout}
+      >
+        Sair
+      </button>
     </div>
   );
 }
